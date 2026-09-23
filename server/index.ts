1732f6cc24e8ddf98196a716e44f261d813e4339
hirokimorino <jungle379@gmail.com>
2026-09-23 12:57:25 +0900

    syuusei1

--- server/index.ts
+++ server/index.ts
@@ -25,7 +25,11 @@ app.onError((error, c) => {
 app.use(
   "/api/*",
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://illustration-todo.vercel.app",
+    ],
   }),
 );
 
